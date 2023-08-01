2a2b1d47bdbf9403b4566d7ba4506765eec301ef
RanaAliAbid <72215018+RanaAliAbid@users.noreply.github.com>
2023-08-01 20:19:58 +0400

    terms and condition, privacy policy, cancellation policy, return policy updates

--- src/pages/cancellation-replacement-policy/index.tsx
+++ src/pages/cancellation-replacement-policy/index.tsx
@@ -23,7 +23,7 @@ export default function Private() {
 
     return (
         <>
-            <div>
+           <div>
                 <ThemeProvider theme={theme}>
                     <main className={styles.main}>
                         <Header title="Home" />
@@ -66,7 +66,7 @@ export default function Private() {
                                             </ListItem>
                                             <ListItem>
                                                 <Typography variant="h5">
-                                                    This Cancellation and Replacement Policy ("this Policy") governs the terms and conditions for canceling an order on the LYOTECH LABS website ("Website," "we," "our") made by the customer ("Customer," "you," "your") before it has been shipped. This Policy should be read in conjunction with the Delivery Policy available on our Website.
+                                                This Cancellation and Replacement Policy (“this Policy”) governs the terms and conditions for canceling an order on the LYOTECH LABS ELECTRONICS TRADING L.L.C website (“Website,” “we,” “our”) made by the customer (“Customer,” “you,” “your”) before it has been shipped. This Policy should be read in conjunction with the Delivery Policy available on our Website.
                                                 </Typography>
                                             </ListItem>
                                         </List>
@@ -82,12 +82,12 @@ export default function Private() {
                                                 <List>
                                                     <ListItem>
                                                         <Typography variant="h5">
-                                                            You have the right to cancel your order before it has been shipped from our warehouse.
+                                                        You have the right to cancel your order before it has been shipped from our warehouse.
                                                         </Typography>
                                                     </ListItem>
                                                     <ListItem>
                                                         <Typography variant="h5">
-                                                            The cancellation of the order shall be made within twenty-four (24) hours from the time of order confirmation.
+                                                        The cancellation of the order shall be made within twenty-four (24) hours from the time of order confirmation.
                                                         </Typography>
                                                     </ListItem>
                                                 </List>
@@ -102,36 +102,34 @@ export default function Private() {
                                                 <List className={styles.orderedListRoman}>
                                                     <ListItem>
                                                         <Typography variant="h5">
-                                                            To request a cancellation, you must contact our customer support team at
-                                                            <a href='mailto:support@lyotechlabs.com'>support@lyotechlabs.com</a>
-                                                            within this timeframe, providing the necessary order details and reason for cancellation.
+                                                        To request a cancellation, you must contact our customer support team at <a href="mailto:support@lyotechlabs.com"> support@lyotechlabs.com </a> within this timeframe, providing the necessary order details and reason for cancellation.
                                                         </Typography>
                                                     </ListItem>
 
                                                     <ListItem>
                                                         <Typography variant="h5">
-                                                            If your cancellation request is received and processed before the order is shipped, subject to Section 1.5, you may indeed be eligible for a refund. In such cases, the amount you paid will be credited back to the original credit or debit card used for the payment.
-                                                        </Typography>
-                                                    </ListItem>
-
-
-                                                    <ListItem>
-                                                        <Typography variant="h5">
-                                                            You shall be aware that during the process of refund, there shall be a deduction of the payment transaction fee or any other applicable fee associated with the payment. This deduction covers any fees incurred during the initial payment transaction.
+                                                        If your cancellation request is received and processed before the order is shipped, subject to Section 1.5, you may indeed be eligible for a refund. In such cases, the amount you paid will be credited back to the original credit or debit card used for the payment.
                                                         </Typography>
                                                     </ListItem>
 
 
                                                     <ListItem>
                                                         <Typography variant="h5">
-                                                            Please refer to our Delivery Policy for information on the expected shipping timeframes.
+                                                        You shall be aware that during the process of refund, there shall be a deduction of the payment transaction fee or any other applicable fee associated with the payment. This deduction covers any fees incurred during the initial payment transaction.
                                                         </Typography>
                                                     </ListItem>
 
 
                                                     <ListItem>
                                                         <Typography variant="h5">
-                                                            In the event that the stipulated time frame mentioned in Section 1.2 has lapsed or if the order has already been shipped, Customers will need to follow the Return and Refund Policy as outlined on our Website. This policy provides details on the process and requirements for returning the product and requesting a refund. It is the customer's responsibility to familiarize themselves with the Return and Refund Policy and follow the necessary steps outlined therein.
+                                                        Please refer to our Delivery Policy for information on the expected shipping timeframes.
+                                                        </Typography>
+                                                    </ListItem>
+
+
+                                                    <ListItem>
+                                                        <Typography variant="h5">
+                                                        In the event that the stipulated time frame mentioned in Section 1.2 has lapsed or if the order has already been shipped, Customers will need to follow the Return and Refund Policy as outlined on our Website. This policy provides details on the process and requirements for returning the product and requesting a refund. It is the customer’s responsibility to familiarize themselves with the Return and Refund Policy and follow the necessary steps outlined therein.
                                                         </Typography>
                                                     </ListItem>
                                                 </List>
